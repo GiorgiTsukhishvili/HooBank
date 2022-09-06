740b8fb5a29f99199736875e1b0d152f8bcf9b43
Giorgi Tsukhishvili <gcuxishvili@gmail.com>
2022-09-06 19:46:11 +0400

    Clients section done

--- src/components/ClientsSection.tsx
+++ src/components/ClientsSection.tsx
@@ -1,7 +1,27 @@
 import React from "react";
 
+import { clients } from "../constents";
+import styles from "../utils/style";
+
 const ClientsSection = () => {
-  return <div>ClientsSection</div>;
+  return (
+    <section id="clients" className={`${styles.flexCenter} my-4`}>
+      <div className={`${styles.flexCenter} flex-wrap w-full`}>
+        {clients.map((client) => (
+          <div
+            key={client.id}
+            className={`flex-1 ${styles.flexCenter} sm:min-w-[192px] min-w-30 transition-all hover:bg-emerald-300 py-3 px-1 rounded-[10px] duration-500`}
+          >
+            <img
+              src={client.logo}
+              alt="Client"
+              className="sm:w-[192px] w-100 h-10 object-contain"
+            />
+          </div>
+        ))}
+      </div>
+    </section>
+  );
 };
 
 export default ClientsSection;
